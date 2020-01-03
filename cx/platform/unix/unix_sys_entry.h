8ac64459b8b12b57465e6adffe8faa9a3dd5d667
Number Six <59003907+Number5ix@users.noreply.github.com>
2020-01-03 06:00:25 +0000

    Fix entry point on unix

--- cx/platform/unix/unix_sys_entry.h
+++ cx/platform/unix/unix_sys_entry.h
@@ -2,8 +2,8 @@
 
 #include "cx/cx.h"
 
-#define DEFINE_ENTRY_POINT              \
-int main(int argc, char *argv[]) {      \
-    _entryParseArgs(argc, argv);        \
-    return entryPoint();                \
+#define DEFINE_ENTRY_POINT               \
+int main(int argc, const char *argv[]) { \
+    _entryParseArgs(argc, argv);         \
+    return entryPoint();                 \
 }
