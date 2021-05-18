508ca65329a6aed5c61ecc4acda9122a9221dcda
Number Six <59003907+Number5ix@users.noreply.github.com>
2021-05-18 14:23:09 +0000

    Rework log thread to spend less time holding locks and to advance the read pointer sooner to make it less likely that the log buffer has to be expanded.

--- cx/log/logthread.c
+++ cx/log/logthread.c
@@ -8,40 +8,49 @@ _Thread_local sa_LogEntry _log_thread_batch;
 
 static int logthread_func(Thread *self)
 {
+    sa_LogEntry ents;
+
     // wait for systhread registration to complete
     while (!_log_event)
         osYield();
 
-    while (!atomicLoad(bool, &self->requestExit, Acquire)) {
-        mutexAcquire(&_log_dests_lock);
-        rwlockAcquireRead(&_log_buffer_lock);
+    saInit(&ents, ptr, 16);
 
+    while (!atomicLoad(bool, &self->requestExit, Acquire)) {
+        rwlockAcquireRead(&_log_buffer_lock);
         int32 bsize = saSize(_log_buffer);      // size cannot change while read lock is held
         int32 rdptr = atomicLoad(int32, &_log_buf_readptr, Relaxed);
         int32 wrptr = atomicLoad(int32, &_log_buf_writeptr, Acquire);
 
-        // step through available log entries
+        // grab available log entries
         while (rdptr != wrptr) {
             LogEntry *ent = atomicLoad(ptr, &_log_buffer.a[rdptr], Acquire);
             devAssert(ent);
             if (!ent)
                 continue;
 
-            foreach(sarray, idx, LogDest*, dest, _log_dests) {
+            saPush(&ents, ptr, ent);
+            atomicStore(ptr, &_log_buffer.a[rdptr], NULL, Release);
+            rdptr = (rdptr + 1) % bsize;
+            atomicStore(int32, &_log_buf_readptr, rdptr, Release);
+        }
+        rwlockReleaseRead(&_log_buffer_lock);
+
+        // now that we have a batch of log entries, process them
+        mutexAcquire(&_log_dests_lock);
+        foreach(sarray, ent__idx, LogEntry*, ent, ents) {
+            foreach(sarray, dest_idx, LogDest*, dest, _log_dests) {
                 if (ent->level <= dest->maxlevel && (!dest->catfilter || dest->catfilter == ent->cat)) {
                     // dispatch to log destination
                     dest->func(ent->level, ent->cat, ent->timestamp, ent->msg, dest->userdata);
                 }
             } endforeach;
-
-            atomicStore(ptr, &_log_buffer.a[rdptr], NULL, Release);
             logDestroyEnt(ent);
-            rdptr = (rdptr + 1) % bsize;
-        }
-        atomicStore(int32, &_log_buf_readptr, rdptr, Release);
-
-        rwlockReleaseRead(&_log_buffer_lock);
+        } endforeach;
         mutexRelease(&_log_dests_lock);
+
+        saClear(&ents);
+
         eventWait(_log_event);
     }
 
