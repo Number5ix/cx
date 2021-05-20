fbedd6b0fb079a28563846675b9ba22fda18fbda
Number Six <59003907+Number5ix@users.noreply.github.com>
2021-05-20 02:55:02 +0000

    Add logfile module

---
+++ cx/log/logfile.c
@@ -0,0 +1,388 @@
+#include "logfile.h"
+#include <cx/fs/path.h>
+#include <cx/container.h>
+#include <cx/time.h>
+#include <cx/format.h>
+
+typedef struct LogFileData {
+    LogFileConfig config;
+    VFS *vfs;
+    string fname;
+    string pathname;
+    string basename;
+    string ext;
+    VFSFile *curfile;
+    int numseen;
+    int64 lastrotate;
+    int64 cursize;
+} LogFileData;
+
+#ifdef _PLATFORM_WIN
+static string loglineend = _S"\r\n";
+#else
+static string loglineend = _S"\n";
+#endif
+
+static void logfileDestroy(LogFileData *data)
+{
+    vfsClose(data->curfile);
+    objRelease(data->vfs);
+    strDestroy(&data->fname);
+    strDestroy(&data->pathname);
+    strDestroy(&data->basename);
+    strDestroy(&data->ext);
+    xaFree(data);
+}
+
+static bool logfileOpen(LogFileData *data)
+{
+    devAssert(!data->curfile);
+    data->curfile = vfsOpen(data->vfs, data->fname, Create, Write);
+    if (!data->curfile)
+        return false;
+    vfsSeek(data->curfile, 0, FS_End);
+    data->cursize = vfsTell(data->curfile);
+
+    return true;
+}
+
+static bool logfileClose(LogFileData *data)
+{
+    devAssert(data->curfile);
+    vfsClose(data->curfile);
+    data->curfile = NULL;
+
+    return true;
+}
+
+LogFileData *logfileCreate(VFS *vfs, strref filename, LogFileConfig *config)
+{
+    LogFileData *ret = xaAlloc(sizeof(LogFileData), Zero);
+    string realfile = 0;
+    if (!ret)
+        return NULL;
+
+    vfsAbsolutePath(vfs, &realfile, filename);
+
+    ret->config = *config;
+    ret->vfs = objAcquire(vfs);
+    strDup(&ret->fname, realfile);
+
+    // save path breakdown to make rotation easier
+    pathGetExt(&ret->ext, realfile);
+    pathParent(&ret->pathname, realfile);
+    pathFilename(&ret->basename, realfile);
+    pathRemoveExt(&ret->basename, ret->basename);
+    strDestroy(&realfile);
+
+    if (strEmpty(ret->ext)) {
+        pathAddExt(&ret->fname, ret->fname, _S"log");
+        strDup(&ret->ext, _S"log");
+    }
+
+    // for time-based rotation need to figure out the last rotate date based on
+    // the modify timestamp to make sure it works correctly across sessions
+    FSStat stat;
+    if (config->rotateMode == LOG_RotateTime && vfsStat(vfs, ret->fname, &stat) == FS_File) {
+        int64 now = (config->flags & LOG_LocalTime) ? clockWallLocal() : clockWall();
+        if (config->flags & LOG_LocalTime)
+            stat.modified = timeLocal(stat.modified, NULL);
+
+        // calculate the last rotation time before the file's modify timestamp
+        TimeParts tp;
+        timeDecompose(&tp, stat.modified);
+        tp.hour = config->rotateHour;
+        tp.minute = config->rotateMinute;
+        tp.second = config->rotateSecond;
+        ret->lastrotate = timeCompose(&tp);
+
+        // if the file was modified before the rotation time, assume it must have been rotated the previous day
+        if (stat.modified < ret->lastrotate)
+            ret->lastrotate -= timeS(86400);
+    }
+
+    if (!logfileOpen(ret)) {
+        logfileDestroy(ret);
+        return NULL;
+    }
+
+    return ret;
+}
+
+static void deleteOldFiles(LogFileData *lfd)
+{
+    FSSearchIter fsi;
+    string pattern = 0, temp = 0;
+    sa_string todelete, splits;
+
+    // now must be in UTC because it's being compared to the file timestamps
+    int64 now = clockWall();
+
+    strNConcat(&pattern, lfd->basename, _S".*.", lfd->ext);
+    saInit(&todelete, string, 4);
+    saInit(&splits, string, 4);
+
+    // count the number of rotated (NON-DATE) files so that unlimited retention can work...
+    // unlimited up to 10k files anyway
+    lfd->numseen = 0;
+
+    vfsSearchInit(&fsi, lfd->vfs, lfd->pathname, pattern, FS_File, true);
+    while (vfsSearchValid(&fsi)) {
+        if (lfd->config.rotateKeepTime > 0 &&
+            (now - fsi.stat.modified) > lfd->config.rotateKeepTime) {
+            saPush(&todelete, string, fsi.name);
+        } else if (strSplit(&splits, fsi.name, _S".", false) == 3) {
+            int num;
+            if (strToInt32(&num, splits.a[1], 10, true) && num < 10000) {
+                // this is a numbered file, not one with a date
+                // delete the last one in the sequence so the others can be renumbered
+                if (lfd->config.rotateKeepFiles > 0 && num >= lfd->config.rotateKeepFiles) {
+                    saPush(&todelete, string, fsi.name);
+                } else {
+                    lfd->numseen = max(lfd->numseen, num);
+                }
+            }
+        }
+        vfsSearchNext(&fsi);
+    }
+    vfsSearchFinish(&fsi);
+
+    foreach(sarray, idx, string, fn, todelete) {
+        pathJoin(&temp, lfd->pathname, fn);
+        vfsDelete(lfd->vfs, temp);
+    } endforeach;
+
+    saDestroy(&todelete);
+    saDestroy(&splits);
+    strDestroy(&pattern);
+    strDestroy(&temp);
+}
+
+static void doSizeRotation(LogFileData *lfd)
+{
+    if (lfd->config.rotateSize == 0 || lfd->cursize < lfd->config.rotateSize)
+        return;         // file isn't big enough yet
+
+    logfileClose(lfd);
+    deleteOldFiles(lfd);
+
+    int nfiles = lfd->config.rotateKeepFiles > 0 ? lfd->config.rotateKeepFiles : lfd->numseen + 1;
+
+    string namei = 0, nameimo = 0;
+    for (int i = nfiles; i >= 1; --i) {
+        strFormat(&namei, _S"${string}/${string}.${int}.${string}",
+                  stvar(string, lfd->pathname),
+                  stvar(string, lfd->basename),
+                  stvar(int32, i),
+                  stvar(string, lfd->ext));
+        if (i > 1) {
+            strFormat(&nameimo, _S"${string}/${string}.${int}.${string}",
+                      stvar(string, lfd->pathname),
+                      stvar(string, lfd->basename),
+                      stvar(int32, i - 1),
+                      stvar(string, lfd->ext));
+        } else {
+            strDup(&nameimo, lfd->fname);
+        }
+
+        if (vfsExist(lfd->vfs, namei))
+            vfsDelete(lfd->vfs, namei);
+        if (vfsExist(lfd->vfs, nameimo))
+            vfsRename(lfd->vfs, nameimo, namei);
+    }
+    logfileOpen(lfd);
+
+    strDestroy(&namei);
+    strDestroy(&nameimo);
+}
+
+static void doTimeRotation(LogFileData *lfd)
+{
+    int64 lastrotate = lfd->lastrotate;
+
+    // now must be in local time because that's how rotateTime is specified when LOG_LocalTime is set
+    int64 now = (lfd->config.flags & LOG_LocalTime) ? clockWallLocal() : clockWall();
+
+    // figure out next rotation time, which will time from the config
+    // on the day after the last rotation
+    TimeParts tp;
+    timeDecompose(&tp, lastrotate + timeS(86400));
+
+    tp.hour = lfd->config.rotateHour;
+    tp.minute = lfd->config.rotateMinute;
+    tp.second = lfd->config.rotateSecond;
+    int64 nextrotate = timeCompose(&tp);
+
+    if (now < nextrotate)
+        return;         // not time to rotate yet
+
+    logfileClose(lfd);
+
+    // use last rotation time for date in filename
+    timeDecompose(&tp, lastrotate);
+    deleteOldFiles(lfd);
+    lfd->lastrotate = now;
+
+    string rfname = 0;
+    strFormat(&rfname, _S"${string}/${string}.${int}.${string}",
+              stvar(string, lfd->pathname),
+              stvar(string, lfd->basename),
+              stvar(int32, tp.year * 10000 + tp.month * 100 + tp.day),
+              stvar(string, lfd->ext));
+    vfsRename(lfd->vfs, lfd->fname, rfname);
+    strDestroy(&rfname);
+
+    logfileOpen(lfd);
+}
+
+static void checkRotate(LogFileData *lfd)
+{
+    if (lfd->config.rotateMode == LOG_RotateSize)
+        doSizeRotation(lfd);
+    else if (lfd->config.rotateMode == LOG_RotateTime)
+        doTimeRotation(lfd);
+}
+
+static void formatDate(LogFileData *lfd, string *out, int64 timestamp)
+{
+    int64 toffsetraw = 0;
+    TimeParts tp = { 0 };
+    if (lfd->config.flags & LOG_LocalTime) {
+        timestamp = timeLocal(timestamp, &toffsetraw);
+    }
+
+    int toffset = (int32)timeToSeconds(toffsetraw) / 60;        // need offset in minutes for formatting
+    timeDecompose(&tp, timestamp);
+
+    switch (lfd->config.dateFormat) {
+    case LOG_DateISO:
+        if (toffset != 0) {
+            // ISO8601 with time zone
+            strFormat(out, _S"${0int(4)}-${0uint(2)}-${0uint(2)}T${0uint(2)}:${0uint(2)}:${0uint(2)}${+int(min:2)}:${0int(2)}",
+                      stvar(int32, tp.year),
+                      stvar(uint8, tp.month),
+                      stvar(uint8, tp.day),
+                      stvar(uint8, tp.hour),
+                      stvar(uint8, tp.minute),
+                      stvar(uint8, tp.second),
+                      stvar(int32, toffset / 60),
+                      stvar(int32, (toffset >= 0 ? toffset : -toffset) % 60));
+        } else {
+            // ISO8601 with zulu time
+            strFormat(out, _S"${0int(4)}-${0uint(2)}-${0uint(2)}T${0uint(2)}:${0uint(2)}:${0uint(2)}Z",
+                      stvar(int32, tp.year),
+                      stvar(uint8, tp.month),
+                      stvar(uint8, tp.day),
+                      stvar(uint8, tp.hour),
+                      stvar(uint8, tp.minute),
+                      stvar(uint8, tp.second));
+        }
+        break;
+    case LOG_DateISOCompact:
+        // simplifed ISO-like format with no time zone
+        strFormat(out, _S"${0int(4)}-${0uint(2)}-${0uint(2)} ${0uint(2)}:${0uint(2)}:${0uint(2)}",
+                  stvar(int32, tp.year),
+                  stvar(uint8, tp.month),
+                  stvar(uint8, tp.day),
+                  stvar(uint8, tp.hour),
+                  stvar(uint8, tp.minute),
+                  stvar(uint8, tp.second));
+        break;
+    case LOG_DateNCSA:
+        // NCSA common log date format
+        strFormat(out, _S"${0uint(2)}/${string(3)}/${0int(4)}:${0uint(2)}:${0uint(2)}:${0uint(2)} ${+int(min:2)}${0int(2)}",
+                  stvar(uint8, tp.day),
+                  stvar(strref, timeMonthAbbrev[tp.month]),
+                  stvar(int32, tp.year),
+                  stvar(uint8, tp.hour),
+                  stvar(uint8, tp.minute),
+                  stvar(uint8, tp.second),
+                  stvar(int32, toffset / 60),
+                  stvar(int32, (toffset >= 0 ? toffset : -toffset) % 60));
+        break;
+    case LOG_DateSyslog:
+        // BSD-style syslog format (without year)
+        strFormat(out, _S"${string(3)} ${uint(2)} ${0uint(2)}:${0uint(2)}:${0uint(2)}",
+                  stvar(strref, timeMonthAbbrev[tp.month]),
+                  stvar(uint8, tp.day),
+                  stvar(uint8, tp.hour),
+                  stvar(uint8, tp.minute),
+                  stvar(uint8, tp.second));
+        break;
+    }
+}
+
+// this function is always called from the log thread and does not need to worry about concurrency
+void logfileDest(int level, LogCategory *cat, int64 timestamp, strref msg, void *userdata)
+{
+    LogFileData *lfd = (LogFileData*)userdata;
+    if (!lfd)
+        return;
+
+    if (level == -1) {
+        // we're being asked to close the log and quit
+        logfileDestroy(lfd);
+        return;
+    }
+
+    checkRotate(lfd);
+
+    string logline = 0;
+    string logdate = 0, loglevel = 0, logcat = 0, logspaces = 0;
+
+    int nspaces = lfd->config.spacing ? lfd->config.spacing : 2;
+    char *sbuf = strBuffer(&logspaces, nspaces + (lfd->config.flags & LOG_AddColon ? 1 : 0));
+    memset(sbuf, ' ', nspaces);
+    if (lfd->config.flags & LOG_AddColon)
+        sbuf[0] = ':';
+
+    formatDate(lfd, &logdate, timestamp);
+
+    // add level prefix
+    if (!(lfd->config.flags & LOG_OmitLevel)) {
+        strref *lvarr = (lfd->config.flags & LOG_ShortLevel) ? LogLevelAbbrev : LogLevelNames;
+        int lvmaxlen = (lfd->config.flags & LOG_ShortLevel) ? 1 : 7;
+        if (lfd->config.flags & LOG_BracketLevel) {
+            if (lfd->config.flags & LOG_JustifyLevel) {
+                // justified with brackets... yuck
+                int llen = strLen(lvarr[level]);
+                char *temp = strBuffer(&loglevel, lvmaxlen + 3);
+                memset(temp, ' ', lvmaxlen + 3);
+                temp[1] = '[';
+                temp[llen + 2] = ']';
+                memcpy(temp + 2, strC(lvarr[level]), llen);
+            } else {
+                strFormat(&loglevel, _S" [${string}]", stvar(strref, lvarr[level]));
+            }
+        } else if (lfd->config.flags & LOG_JustifyLevel) {
+            if (lfd->config.flags & LOG_ShortLevel) {
+                strConcat(&loglevel, _S" ", lvarr[level]);
+            } else {
+                strFormat(&loglevel, _S" ${string(7)}", stvar(strref, lvarr[level]));
+            }
+        } else {
+            strConcat(&loglevel, _S" ", lvarr[level]);
+        }
+    }
+
+    if (lfd->config.flags & LOG_IncludeCategory && cat && !strEmpty(cat->name)) {
+        if (lfd->config.flags & LOG_BracketCategory) {
+            strFormat(&logcat, _S" [${string}]", stvar(strref, cat->name));
+        } else {
+            strConcat(&logcat, _S" ", cat->name);
+        }
+    }
+
+    if (lfd->config.flags & LOG_CategoryFirst)
+        strNConcat(&logline, logdate, logcat, loglevel, logspaces, msg, loglineend);
+    else
+        strNConcat(&logline, logdate, loglevel, logcat, logspaces, msg, loglineend);
+    strDestroy(&logdate);
+    strDestroy(&loglevel);
+    strDestroy(&logcat);
+    strDestroy(&logspaces);
+
+    vfsWrite(lfd->curfile, (void*)strC(logline), strLen(logline), NULL);
+    lfd->cursize += strLen(logline);
+    strDestroy(&logline);
+}
---
+++ cx/log/logfile.h
@@ -0,0 +1,49 @@
+#include <cx/log/log.h>
+#include <cx/fs/vfs.h>
+
+enum LOG_DATE_FORMATS {
+    LOG_DateISO,
+    LOG_DateISOCompact,
+    LOG_DateNCSA,
+    LOG_DateSyslog
+};
+
+enum LOG_FLAGS {
+    LOG_LocalTime           =  0x0001,          // timestamps in local time rather than UTC
+    LOG_OmitLevel           =  0x0002,          // do not include severity level
+    LOG_ShortLevel          =  0x0004,          // Use log level abbreviations
+    LOG_BracketLevel        =  0x0008,          // Enclose log level in brackets
+    LOG_JustifyLevel        =  0x0010,          // Make the level a column of consistent length
+    LOG_IncludeCategory     =  0x0020,          // Include category name
+    LOG_BracketCategory     =  0x0040,          // Put category name in brackets
+    LOG_AddColon            =  0x0080,          // Add a colon after the prefix
+    LOG_CategoryFirst       =  0x0100,          // category comes between date and level instead of at the end
+};
+
+enum LOG_ROTATE_MODE {
+    LOG_RotateSize = 1,     // Rotate when file size exceeds rotateSize
+    LOG_RotateTime,         // Rotate at a specific time of day
+};
+
+typedef struct LogFileConfig {
+    int dateFormat;
+    int rotateMode;
+    int spacing;            // spaces between message prefix and message (default: 2)
+
+    uint32 flags;
+
+    int64 rotateSize;
+    uint8 rotateHour;
+    uint8 rotateMinute;
+    uint8 rotateSecond;
+
+    int rotateKeepFiles;    // minimum number of files to keep after rotation
+    int64 rotateKeepTime;   // minimum length of time to keep rotated log files
+} LogFileConfig;
+
+typedef struct LogFileData LogFileData;
+
+LogFileData *logfileCreate(VFS *vfs, strref filename, LogFileConfig *config);
+
+// for use with logRegisterDest along with the userdata returned from logfileCreate
+void logfileDest(int level, LogCategory *cat, int64 timestamp, strref msg, void *userdata);
