ad4cf4ea84f614a65ae4ed8d6178c4ef1efbb764
Number Six <59003907+Number5ix@users.noreply.github.com>
2021-06-10 15:35:38 +0000

    Add tokstring macro to stringize a token

--- cx/utils/macros.h
+++ cx/utils/macros.h
@@ -6,6 +6,8 @@
 #define tokconcat(x,y) tokconcat2(x,y)
 #define tokconcatU2(x,y) x##_##y
 #define tokconcatU(x,y) tokconcatU2(x,y)
+#define tokstring2(x) #x
+#define tokstring(x) tokstring2(x)
 
 #define _get_nth_arg_50(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, N, ...) N
 // extra indirection macro to workaround MSVC brokenness
