c0d07e2ecf8ad108eb726962436c2afa0009869a
Number Six <59003907+Number5ix@users.noreply.github.com>
2022-04-26 20:51:31 +0000

    Changes to thread module needed for WebAssembly

--- cx/platform/wasm/wasm_thread_thread.c
+++ cx/platform/wasm/wasm_thread_thread.c
@@ -1,12 +1,7 @@
 #include <cx/thread.h>
-#if defined(_PLATFORM_FBSD)
-#include <pthread.h>
-#include <pthread_np.h>
-#elif defined(_PLATFORM_LINUX)
 #define _GNU_SOURCE
 #define __USE_GNU
 #include <pthread.h>
-#endif
 #include <sched.h>
 
 typedef struct UnixThread {
@@ -83,84 +78,7 @@ bool _thrPlatformWait(Thread *thread, int64 timeout)
     return thr->joined;
 }
 
-#ifdef _PLATFORM_LINUX
-// Linux pthreads sucks because it doesn't have any higher-than-normal
-// priority levels other than realtime. It can do per-thread nice (maybe,
-// seems to depend on kernel version and particular implementation) which
-// would work except that there isn't a way to set it for anthing other
-// than the current thread. That makes things awkward and doesn't fit well
-// with our API. For now we just don't support THREAD_High or Higher.
+// WebAssembly doesn't support thread priority at all
 bool _thrPlatformSetPriority(Thread *thread, int prio) {
-    UnixThread *thr = (UnixThread*)thread;
-
-    struct sched_param param = {0};
-    int policy = SCHED_OTHER;
-
-    switch(prio) {
-        case THREAD_Normal:
-            policy = SCHED_OTHER;
-            break;
-        case THREAD_Batch:
-            policy = SCHED_BATCH;
-            break;
-        case THREAD_Low:
-            policy = SCHED_IDLE;
-            break;
-        case THREAD_Idle:
-            policy = SCHED_IDLE;
-            break;
-        case THREAD_High:
-            policy = SCHED_OTHER;
-            break;
-        case THREAD_Higher:
-            policy = SCHED_OTHER;
-            break;
-        case THREAD_Realtime:
-            policy = SCHED_RR;
-            param.sched_priority = 1;
-            break;
-    }
-
-    return !pthread_setschedparam(thr->pthr, policy, &param);
+    return false;
 }
-#endif
-
-#ifdef _PLATFORM_FBSD
-// FreeBSD pthreads sucks because it doesn't have any *lower*-than-normal
-// priority levels. It does support priority levels on SCHED_OTHER though,
-// so we can sort of fake it with that.
-bool _thrPlatformSetPriority(Thread *thread, int prio) {
-    UnixThread *thr = (UnixThread*)thread;
-
-    struct sched_param param = {0};
-    int policy = SCHED_OTHER;
-    int maxprio = sched_get_priority_max(SCHED_OTHER);
-
-    switch(prio) {
-        case THREAD_Normal:
-            param.sched_priority = maxprio / 2;
-            break;
-        case THREAD_Batch:
-            param.sched_priority = maxprio / 2 - 1;
-            break;
-        case THREAD_Low:
-            param.sched_priority = maxprio / 4;
-            break;
-        case THREAD_Idle:
-            param.sched_priority = sched_get_priority_min(SCHED_OTHER);
-            break;
-        case THREAD_High:
-            param.sched_priority = maxprio / 4 * 3;
-            break;
-        case THREAD_Higher:
-            param.sched_priority = maxprio;
-            break;
-        case THREAD_Realtime:
-            policy = SCHED_RR;
-            param.sched_priority = 1;
-            break;
-    }
-
-    return !pthread_setschedparam(thr->pthr, policy, &param);
-}
-#endif
