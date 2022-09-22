ce8d7496bee3df36aec69177e774a900caa642fe
Number Six <59003907+Number5ix@users.noreply.github.com>
2022-09-22 12:45:46 +0000

    Migrate events to use futexes
    Small API changes with regards to alertable events (renamed
    from EV_PlatformEvents).
    
    Slightly different semantics for multiple/all wakeup cases, but more in
    line with de facto standards.

--- cx/thread/event.c
+++ cx/thread/event.c
@@ -1,76 +1,155 @@
 #include "event.h"
+#include <cx/time/time.h>
+#include <cx/utils/compare.h>
+#include <cx/platform/os.h>
 
 bool _eventInit(Event *e, flags_t flags)
 {
-    memset(e, 0, sizeof(Event));
-    semaInit(&e->sema, 0);
+    int32 ftxflags = 0;
+    if (flags & EV_Alertable)
+        ftxflags = FUTEX_Alertable;
+    futexInit(&e->ftx, 0, ftxflags);
+    atomicStore(int32, &e->waiters, 0, Relaxed);
 
-    // Default to no-spin semaphore unless requested otherwise
+    // Default to not spin unless requested otherwise
     // See the comments in event.h
-    if (!(flags & EV_Spin))
-        e->sema.flags |= SEMA_NoSpin;
-    if (flags & EV_PlatformEvents)
-        e->sema.flags |= SEMA_PlatformEvents;
+    aspinInit(&e->aspin, !(flags & EV_Spin));
     return true;
 }
 
 bool eventSignalMany(Event *e, int32 count)
 {
-    int32 waiting = atomicLoad(int32, &e->waiting, Relaxed);
+    bool ret = false;
+    devAssert(count > 0);
+    AdaptiveSpinState astate;
+    aspinBegin(&e->aspin, &astate, timeForever);
 
-    // reduce number of theads waiting, working our way towards -1 (signaled but no waiters)
-    while (waiting != -2 && !atomicCompareExchange(int32, weak, &e->waiting, &waiting,
-                                                   clamplow(waiting - count, -1), Release, Relaxed)) {}
-    // actually release the waiting thread(s)
-    if (waiting > 0)
-        semaInc(&e->sema, min(waiting, count));
+    // get the number of waiting threads and reduce it by the amount we're about to wake up
+    int32 waiters = atomicLoad(int32, &e->waiters, Relaxed);
+    while (!atomicCompareExchange(int32, weak, &e->waiters, &waiters, clamplow(waiters - count, 0), Relaxed, Relaxed)) {
+        aspinHandleContention(&e->aspin, &astate);
+    }
 
-    // return false if it was already signaled
-    return (waiting >= 0);
+    // can only wake up as many as threads are currently waiting
+    count = clamphigh(count, waiters);
+
+    // Set the futex value, which we're using kind of like a semaphore, to the number that needs to wake up.
+    // Normally we want this to behave like a binary semaphore, so if the event is signaled but nothing has
+    // waited yet (i.e. they are currently in the process of waking up), we don't want to signal it again.
+    // Because the event should end up in a signaled state when this is called, enforce a minimum of 1 to set
+    // the futex value to, even if there are no waiters.
+    int32 val = atomicLoad(int32, &e->ftx.val, Acquire);
+    while (val >= 0 && !atomicCompareExchange(int32, weak, &e->ftx.val, &val, clamplow(val + count, 1), Release, Relaxed)) {
+        aspinHandleContention(&e->aspin, &astate);
+    }
+
+    while (count) {
+        ret = true;
+        futexWake(&e->ftx);
+        --count;
+    }
+
+    // return true if we woke something up or signaled the event
+    return ret || val == 0;
 }
 
 bool eventSignalAll(Event *e)
 {
-    return eventSignalMany(e, 0x7fffffff);
+    AdaptiveSpinState astate;
+    aspinBegin(&e->aspin, &astate, timeForever);
+
+    // get the number of waiting threads and set it to 0
+    int32 waiters = atomicLoad(int32, &e->waiters, Relaxed);
+    while (waiters > 0 && !atomicCompareExchange(int32, weak, &e->waiters, &waiters, 0, Relaxed, Relaxed)) {
+        aspinHandleContention(&e->aspin, &astate);
+    }
+
+    // set the futex value the number of waiters so they all wake up.
+    // This is a broadcast event, so we don't need to worry about a race between the previous atomic
+    // and this one -- any new waiters after we read the number will just have to wait.
+    int32 val = atomicLoad(int32, &e->ftx.val, Acquire);
+    while (val >= 0 && !atomicCompareExchange(int32, weak, &e->ftx.val, &val, val + waiters, Release, Relaxed)) {
+        aspinHandleContention(&e->aspin, &astate);
+    }
+
+    // let the herd come thundering
+    if (waiters > 0) {
+        futexWakeAll(&e->ftx);
+        return true;
+    }
+
+    return false;
 }
 
 bool eventSignalLock(Event *e)
 {
-    int32 waiting;
+    atomicStore(int32, &e->ftx.val, -1, Release);
+    eventSignalAll(e);
 
-    waiting = atomicLoad(int32, &e->waiting, Relaxed);
+    return true;
+}
 
-    // -2 is the special value that indicates the event is locked, if it ever becomes -2 during this
-    // loop, it means another thread locked it
-    while (waiting != -2) {
-        // try to go to -2, but only if we're already in a signaled state with no threads waiting (-1)
-        if (waiting == -1 && atomicCompareExchange(int32, strong, &e->waiting, &waiting, -2, Release, Relaxed))
-            return true;
-
-        // otherwise try to move towards a signaled state
-        if (waiting >= 0)
-            eventSignalAll(e);
-
-        waiting = atomicLoad(int32, &e->waiting, Relaxed);
+bool eventWaitTimeout(Event *e, uint64 timeout)
+{
+    // try fast path first
+    int32 val = 1;
+    if (atomicCompareExchange(int32, weak, &e->ftx.val, &val, 0, Acquire, Relaxed)) {
+        aspinRecordUncontended(&e->aspin);
+        return true;
     }
 
-    // another thread beat us to it
-    return false;
+    AdaptiveSpinState astate;
+    aspinBegin(&e->aspin, &astate, timeout);
+
+    while (val <= 0 || !atomicCompareExchange(int32, weak, &e->ftx.val, &val, val - 1, Acquire, Relaxed)) {
+        if (val == -1)
+            return true;        // immediate out if event is locked, don't update any stats
+
+        // otherwise need to wait
+        if (val == 0) {
+            if (aspinTimeout(&e->aspin, &astate))
+                return false;
+
+            if (!aspinSpin(&e->aspin, &astate)) {
+                // track waiters for wakeup purposes
+                atomicFetchAdd(int32, &e->waiters, 1, Relaxed);
+                int fret = futexWait(&e->ftx, val, aspinTimeoutRemaining(&astate));
+                if (fret == FUTEX_Alerted || fret == FUTEX_Timeout) {
+                    // Wait finished early: need to undo the add above, but be careful because another
+                    // thread may have signaled the event in the meantime. This MAY produce a spurious
+                    // wakeup of another thread if we lose the race with eventSignalMany. Keep that in
+                    // mind when waiting on events that may have eventWaitTimeout called on them.
+                    int32 waiters = atomicLoad(int32, &e->waiters, Relaxed);
+                    while (waiters > 0 && !atomicCompareExchange(int32, weak, &e->waiters, &waiters,
+                                                                 waiters - 1, Relaxed, Relaxed)) {
+                        aspinHandleContention(&e->aspin, &astate);
+                    }
+                }
+                if (fret == FUTEX_Alerted)
+                    return true;    // immediate out if we were alerted, don't update spin stats
+            }
+
+            val = atomicLoad(int32, &e->ftx.val, Relaxed);
+        }
+    }
+
+    aspinAdapt(&e->aspin, &astate);
+    return true;
 }
 
 bool eventReset(Event *e)
 {
-    int32 waiting = atomicLoad(int32, &e->waiting, Relaxed);
+    int32 val = atomicLoad(int32, &e->ftx.val, Relaxed);
 
     do {
-        if (waiting >= 0)
+        if (val >= 0)
             return false;
-    } while (!atomicCompareExchange(int32, weak, &e->waiting, &waiting, 0, Release, Relaxed));
+    } while (!atomicCompareExchange(int32, weak, &e->ftx.val, &val, 0, Release, Relaxed));
 
     return true;
 }
 
 void eventDestroy(Event *e)
 {
-    semaDestroy(&e->sema);
+    memset(e, 0, sizeof(Event));
 }
--- cx/thread/event.h
+++ cx/thread/event.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <cx/cx.h>
-#include <cx/platform/base.h>
-#include <cx/utils/compare.h>
-#include "sema.h"
+#include "futex.h"
+#include "aspin.h"
 
 // Event: Similar to auto-reset events on Windows.
-// Basically a semaphore that has a maximum value of 1.
+// Basically a semaphore that has a maximum value of 1, but includes some extra
+// features such as releasing an arbitrary number of threads on demand.
 
 // Also can be locked in a signaled state that turns it into the equivalent of
 // a manual-reset event. This is useful for things like thread shutdown, where
@@ -15,18 +15,22 @@
 CX_C_BEGIN
 
 typedef struct Event {
-    // number of threads waiting for the event
-    // -1 indicates that the event has been signaled
-    atomic(int32) waiting;
-    Semaphore sema;
+    // Futex values:
+    //  0 - Event is not signaled, any thread will have to wait
+    //  1 - Event is signaled
+    // >1 - Event is signaled for multiple waiters to wake up
+    // -1 - Event is locked, all waits will complete instantly
+    Futex ftx;
+    atomic(int32) waiters;
+    AdaptiveSpin aspin;
 } Event;
 
 enum EVENTINITFUNC_FLAGS {
-    EV_Spin = 1,
-    EV_PlatformEvents = 2,
+    EV_Spin = 1,            // Use adaptive spinloop instead of going straight to sleep
+    EV_Alertable = 2,       // May be woken up early by platform-specific events
 };
 
-// Events normally use the "NoSpin" type semaphores.
+// Events normally do not use the adaptive spin framework.
 // This is because we assume that events are used for occasional signaling between threads,
 // and when a thread waits on an event it's more likely than not to need to sleep. It would
 // be a waste of CPU for the event to spin waiting for a signal that is very unlikely to
@@ -34,7 +38,7 @@ enum EVENTINITFUNC_FLAGS {
 //
 // For events used by high-performance queues or other situations where it is expected that
 // there will usually be work to do, the caller should initialize the Event with a "Spin" flag,
-// which will cause the event to use the normal adapative semaphore.
+// which will cause the event to use adaptive spinning.
 bool _eventInit(Event *e, uint32 flags);
 #define eventInit(e, ...) _eventInit(e, opt_flags(__VA_ARGS__))
 
@@ -42,48 +46,17 @@ bool _eventInit(Event *e, uint32 flags);
 bool eventSignalMany(Event *e, int32 count);
 bool eventSignalAll(Event *e);
 
+bool eventWaitTimeout(Event *e, uint64 timeout);
+_meta_inline bool eventWait(Event *e)
+{
+    return eventWaitTimeout(e, timeForever);
+}
+
 // signals the event and locks it in the signaled state, so threads attempting to wait on it
 // always return immediately
 bool eventSignalLock(Event *e);
 // manually reset the event to an unsignaled state
 bool eventReset(Event *e);
-
-_meta_inline bool eventWait(Event *e)
-{
-    int32 waiting = atomicLoad(int32, &e->waiting, Relaxed);
-    while (waiting != -2 && !atomicCompareExchange(int32, weak, &e->waiting, &waiting,
-                                                   waiting + 1, Acquire, Acquire)) {}
-
-    // if event was signaled, return immediately
-    if (waiting < 0)
-        return true;
-
-    return semaDec(&e->sema);
-}
-
-_meta_inline bool eventWaitTimeout(Event *e, uint64 timeout)
-{
-    int32 waiting = atomicLoad(int32, &e->waiting, Relaxed);
-    while (waiting != -2 && !atomicCompareExchange(int32, weak, &e->waiting, &waiting,
-                                                   waiting + 1, Acquire, Acquire)) {}
-
-    // if event was signaled, return immediately
-    if (waiting < 0)
-        return true;
-
-    // try to wait
-    if (semaTryDecTimeout(&e->sema, timeout))
-        return true;
-
-    // Wait timed out: need to undo the add above, but be careful because another
-    // thread may have signaled the event in the meantime. This MAY produce a spurious
-    // wakeup of another thread if we lose the race with eventSignalMany. Keep that in
-    // mind when waiting on events that may have eventWaitTimeout called on them.
-    while (waiting != -2 && !atomicCompareExchange(int32, weak, &e->waiting, &waiting,
-                                  clamplow(waiting - 1, -1), Release, Relaxed)) {}
-    return false;
-}
-
 void eventDestroy(Event *e);
 
 CX_C_END
