9eb6b5361c9f9a76ebd26aea6cf9289d779dcc4f
Number Six <59003907+Number5ix@users.noreply.github.com>
2023-07-19 07:33:00 +0000

    Change SSDNode interface to not be unbound
    This will be needed for json serialization to be able to extend the
    object and override some of the methods.

--- cx/ssdtree/ssdnode.h
+++ cx/ssdtree/ssdnode.h
@@ -8,8 +8,29 @@
 typedef struct SSDNode SSDNode;
 saDeclarePtr(SSDNode);
 
+typedef struct SSDNode_ClassIf {
+    ObjIface *_implements;
+    ObjIface *_parent;
+    size_t _size;
+
+    // Retrieves a child node with the given name, creating it if create is set
+    SSDNode *(*getChild)(void *self, strref name, bool create, SSDLock *lock);
+    // Gets a value. Caller owns the value and must destroy it with stDestroy!
+    bool (*getValue)(void *self, strref name, stvar *out, SSDLock *lock);
+    // Gets a pointer to a value. This points to the hastable entry in children,
+    // so it is only guaranteed to be valid while the read lock is held.
+    stvar *(*getPtr)(void *self, strref name, SSDLock *lock);
+    // Sets the given value
+    void (*setValue)(void *self, strref name, stvar val, SSDLock *lock);
+    // Same as setValue but consumes the value
+    void (*setValueC)(void *self, strref name, stvar *val, SSDLock *lock);
+    // Remove a value or child node
+    bool (*removeValue)(void *self, strref name, SSDLock *lock);
+} SSDNode_ClassIf;
+extern SSDNode_ClassIf SSDNode_ClassIf_tmpl;
+
 typedef struct SSDNode {
-    ObjIface *_;
+    SSDNode_ClassIf *_;
     union {
         ObjClassInfo *_clsinfo;
         void *_is_SSDNode;
@@ -28,45 +49,33 @@ extern ObjClassInfo SSDNode_clsinfo;
 #define SSDNode(inst) ((SSDNode*)((void)((inst) && &((inst)->_is_SSDNode)), (inst)))
 #define SSDNodeNone ((SSDNode*)NULL)
 
-SSDNode *SSDNode_getChild(SSDNode *self, strref name, bool create, SSDLock *lock);
-// SSDNode *ssdnodeGetChild(SSDNode *self, strref name, bool create, SSDLock *lock);
-//
-// Retrieves a child node with the given name, creating it if create is set
-#define ssdnodeGetChild(self, name, create, lock) SSDNode_getChild(SSDNode(self), name, create, lock)
-
-bool SSDNode_getValue(SSDNode *self, strref name, stvar *out, SSDLock *lock);
-// bool ssdnodeGetValue(SSDNode *self, strref name, stvar *out, SSDLock *lock);
-//
-// Gets a value. Caller owns the value and must destroy it with stDestroy!
-#define ssdnodeGetValue(self, name, out, lock) SSDNode_getValue(SSDNode(self), name, out, lock)
-
-stvar *SSDNode_getPtr(SSDNode *self, strref name, SSDLock *lock);
-// stvar *ssdnodeGetPtr(SSDNode *self, strref name, SSDLock *lock);
-//
-// Gets a pointer to a value. This points to the hastable entry in children,
-// so it is only guaranteed to be valid while the read lock is held.
-#define ssdnodeGetPtr(self, name, lock) SSDNode_getPtr(SSDNode(self), name, lock)
-
-void SSDNode_setValue(SSDNode *self, strref name, stvar val, SSDLock *lock);
-// void ssdnodeSetValue(SSDNode *self, strref name, stvar val, SSDLock *lock);
-//
-// Sets the given value
-#define ssdnodeSetValue(self, name, val, lock) SSDNode_setValue(SSDNode(self), name, val, lock)
-
-void SSDNode_setValueC(SSDNode *self, strref name, stvar *val, SSDLock *lock);
-// void ssdnodeSetValueC(SSDNode *self, strref name, stvar *val, SSDLock *lock);
-//
-// Same as setValue but consumes the value
-#define ssdnodeSetValueC(self, name, val, lock) SSDNode_setValueC(SSDNode(self), name, val, lock)
-
-bool SSDNode_removeValue(SSDNode *self, strref name, SSDLock *lock);
-// bool ssdnodeRemoveValue(SSDNode *self, strref name, SSDLock *lock);
-//
-// Remove a value or child node
-#define ssdnodeRemoveValue(self, name, lock) SSDNode_removeValue(SSDNode(self), name, lock)
-
 SSDNode *SSDNode_create(SSDInfo *info);
 // SSDNode *ssdnodeCreate(SSDInfo *info);
 #define ssdnodeCreate(info) SSDNode_create(SSDInfo(info))
 
+// SSDNode *ssdnodeGetChild(SSDNode *self, strref name, bool create, SSDLock *lock);
+//
+// Retrieves a child node with the given name, creating it if create is set
+#define ssdnodeGetChild(self, name, create, lock) (self)->_->getChild(SSDNode(self), name, create, lock)
+// bool ssdnodeGetValue(SSDNode *self, strref name, stvar *out, SSDLock *lock);
+//
+// Gets a value. Caller owns the value and must destroy it with stDestroy!
+#define ssdnodeGetValue(self, name, out, lock) (self)->_->getValue(SSDNode(self), name, out, lock)
+// stvar *ssdnodeGetPtr(SSDNode *self, strref name, SSDLock *lock);
+//
+// Gets a pointer to a value. This points to the hastable entry in children,
+// so it is only guaranteed to be valid while the read lock is held.
+#define ssdnodeGetPtr(self, name, lock) (self)->_->getPtr(SSDNode(self), name, lock)
+// void ssdnodeSetValue(SSDNode *self, strref name, stvar val, SSDLock *lock);
+//
+// Sets the given value
+#define ssdnodeSetValue(self, name, val, lock) (self)->_->setValue(SSDNode(self), name, val, lock)
+// void ssdnodeSetValueC(SSDNode *self, strref name, stvar *val, SSDLock *lock);
+//
+// Same as setValue but consumes the value
+#define ssdnodeSetValueC(self, name, val, lock) (self)->_->setValueC(SSDNode(self), name, val, lock)
+// bool ssdnodeRemoveValue(SSDNode *self, strref name, SSDLock *lock);
+//
+// Remove a value or child node
+#define ssdnodeRemoveValue(self, name, lock) (self)->_->removeValue(SSDNode(self), name, lock)
 
