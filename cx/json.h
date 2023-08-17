fee01cf0b4ceec6dfd21f807396aa117aa0baba3
Number Six <59003907+Number5ix@users.noreply.github.com>
2023-08-17 04:31:41 +0000

    Add top-level serialize.h and json.h include files
    For convenience to avoid having to include multiple files
    to get a JSON parser.

---
+++ cx/json.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cx/ssdtree.h>
+#include <cx/serialize.h>
+#include <cx/serialize/jsonparse.h>
+#include <cx/serialize/jsonout.h>
---
+++ cx/serialize.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cx/serialize/streambuf.h>
+#include <cx/serialize/sbfile.h>
+#include <cx/serialize/sbstring.h>
