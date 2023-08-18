8241ec996e239030b8602fa52710527713b955e1
Number Six <59003907+Number5ix@users.noreply.github.com>
2023-08-18 03:06:08 +0000

    Hacky workaround for GCC complaining about unused operands in comma expressions

--- cx/utils/macros/unused.h
+++ cx/utils/macros/unused.h
@@ -1,4 +1,10 @@
 #pragma once
 
+#if defined(__GNUC__)
+#include <stdint.h>
+__attribute__((always_inline)) inline int _unused_helper(__attribute__((ununsed)) intptr_t a) { return 0; }
+#define unused_noeval(x) (_unused_helper(true ? (intptr_t)0 : (intptr_t)(x)))
+#else
 #define unused_noeval(x) (true ? (void)0 : (void)(x))
+#endif
 #define nop_stmt ((void)0)
