96f923fdf652139885da762e7e37c2074c79f748
Number Six <59003907+Number5ix@users.noreply.github.com>
2023-10-09 03:40:20 +0000

    GCC is incredibly annoying
    If it's not "unused parameter" warnings, it's inconsistent attribute
    support. Or unnamed parameters in the declaration suppressing the
    warning in one verison but causing different warnings in another.

--- cx/utils/macros/unused.h
+++ cx/utils/macros/unused.h
@@ -2,7 +2,7 @@
 
 #if !defined(__clang__) && defined(__GNUC__)
 #include <stdint.h>
-__attribute__((always_inline)) inline int _unused_helper(intptr_t) { return 0; }
+__attribute__((always_inline)) inline int _unused_helper(intptr_t _dummy) { (void)_dummy; return 0; }
 #define unused_noeval(x) (_unused_helper(true ? (intptr_t)0 : (intptr_t)(x)))
 #else
 #define unused_noeval(x) (true ? (void)0 : (void)(x))
