0e1f3ea21baa4999c37273f4d6bf21356ab5d7d4
Number Six <59003907+Number5ix@users.noreply.github.com>
2023-10-15 02:14:43 +0000

    strJoin should properly handle NULLs in supplied array
    Code analyzer flagged this and it's a legitimate issue.

--- cx/string/strjoin.c
+++ cx/string/strjoin.c
@@ -21,20 +21,23 @@ bool strJoin(_Inout_ string *o, _In_ sa_string arr, _In_opt_ strref sep)
     uint32 sz = 0;
     for (i = 0; i < arrsize; i++) {
         sz += strLen(arr.a[i]) + (i > 0 ? seplen : 0);
-        encoding &= _strHdr(arr.a[i]) & STR_ENCODING_MASK;
+        if (STR_CHECK_VALID(arr.a[i]))
+            encoding &= _strHdr(arr.a[i]) & STR_ENCODING_MASK;
     }
 
     _strReset(o, sz);
     uint8 *p = _strBuffer(*o);
 
-    seglen = _strFastLen(arr.a[0]);
-    _strFastCopy(arr.a[0], 0, p, seglen);
-    p += seglen;
+    if (STR_CHECK_VALID(arr.a[0])) {
+        seglen = _strFastLen(arr.a[0]);
+        _strFastCopy(arr.a[0], 0, p, seglen);
+        p += seglen;
+    }
     for (i = 1; i < arrsize; i++) {
-        if (STR_CHECK_VALID(arr.a[i])) {
-            _strFastCopy(sep, 0, p, seplen);
-            p += seplen;
+        _strFastCopy(sep, 0, p, seplen);
+        p += seplen;
 
+        if (STR_CHECK_VALID(arr.a[i])) {
             seglen = _strFastLen(arr.a[i]);
             _strFastCopy(arr.a[i], 0, p, seglen);
             p += seglen;
