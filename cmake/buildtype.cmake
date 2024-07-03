b0f5d1b4d18fa19b0cf18ba133647529bf57fedd
Number Six <59003907+Number5ix@users.noreply.github.com>
2024-07-03 03:55:36 +0000

    Add DevNoOpt configuration type
    DevNoOpt disables compiler optimizations for debugging purposes, but
    does not enable all of the allocator sanity checking, lock debugging,
    queue statistics profiling, and extra debug assertions that the full
    Debug configuration does.

--- cmake/buildtype.cmake
+++ cmake/buildtype.cmake
@@ -1,6 +1,6 @@
 # Defaults for the standard CMake build types if we aren't explicitly given a configured type
 if(NOT DEFINED ${CX_BUILD_TYPE})
-    set(CX_BUILD_TYPE $<$<CONFIG:Debug>:Debug>$<$<CONFIG:Dev>:Dev>$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:Release>)
+    set(CX_BUILD_TYPE $<$<CONFIG:Debug>:Debug>$<$<CONFIG:Dev,DevNoOpt>:Dev>$<$<CONFIG:Release,RelWithDebInfo,MinSizeRel>:Release>)
 endif()
 
 set (isdebug "$<STREQUAL:${CX_BUILD_TYPE},Debug>")
