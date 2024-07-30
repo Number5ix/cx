bef2820304053b18df063638235cbf32c7e1cc2f
Number Six <59003907+Number5ix@users.noreply.github.com>
2024-07-30 22:58:58 +0000

    Add ability to specify include dirs for cxobjgen
    Set EXTRA_SIDL_INCLUDE_DIRS to add custom include directories while
    generating file with add_sidl.

--- cmake/sidl.cmake
+++ cmake/sidl.cmake
@@ -2,10 +2,13 @@ if(CMAKE_CROSSCOMPILING)
 find_package(cxobjgen)
 function(add_sidl)
     set_directory_properties(PROPERTIES CLEAN_NO_CUSTOM 1)
+    foreach(idir ${EXTRA_SIDL_INCLUDE_DIRS})
+        set(EXTRA_CXOBJGEN_ARGS ${EXTRA_CXOBJGEN_ARGS} -I${idir})
+    endforeach()
     foreach(arg ${ARGN})
         string(REGEX REPLACE "\\.[^.]*$" "" argbase ${arg})
         add_custom_command(
-            COMMAND cxobjgen -I${CX_TOP_SOURCE_DIR} -f ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
+            COMMAND cxobjgen -I${CX_TOP_SOURCE_DIR} -I${CX_TOP_SOURCE_DIR}/cx/include ${EXTRA_CXOBJGEN_ARGS} -f ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             IMPLICIT_DEPENDS "C" ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${argbase}.h
@@ -15,10 +18,13 @@ endfunction()
 else()
 function(add_sidl)
     set_directory_properties(PROPERTIES CLEAN_NO_CUSTOM 1)
+    foreach(idir ${EXTRA_SIDL_INCLUDE_DIRS})
+        set(EXTRA_CXOBJGEN_ARGS ${EXTRA_CXOBJGEN_ARGS} -I${idir})
+    endforeach()
     foreach(arg ${ARGN})
         string(REGEX REPLACE "\\.[^.]*$" "" argbase ${arg})
         add_custom_command(
-            COMMAND cxobjgen -I${CX_TOP_SOURCE_DIR} -f ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
+            COMMAND cxobjgen -I${CX_TOP_SOURCE_DIR} -I${CX_TOP_SOURCE_DIR}/cx/include ${EXTRA_CXOBJGEN_ARGS} -f ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             DEPENDS cxobjgen
             IMPLICIT_DEPENDS "C" ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
@@ -30,10 +36,13 @@ endif()
 
 function(add_sidl_nodep)
     set_directory_properties(PROPERTIES CLEAN_NO_CUSTOM 1)
+    foreach(idir ${EXTRA_SIDL_INCLUDE_DIRS})
+        set(EXTRA_CXOBJGEN_ARGS ${EXTRA_CXOBJGEN_ARGS} -I${idir})
+    endforeach()
     foreach(arg ${ARGN})
         string(REGEX REPLACE "\\.[^.]*$" "" argbase ${arg})
         add_custom_command(
-            COMMAND cxobjgen -I${CX_TOP_SOURCE_DIR} -f ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
+            COMMAND cxobjgen -I${CX_TOP_SOURCE_DIR} -I${CX_TOP_SOURCE_DIR}/cx/include ${EXTRA_CXOBJGEN_ARGS} -f ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${arg}
             OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${argbase}.h
             )
