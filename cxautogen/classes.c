303eca3b9200479abeaa8f29617fffd389a20840
Number Six <59003907+Number5ix@users.noreply.github.com>
2026-07-28 15:51:49 +0000

    cxautogen fix: Clear extern an overridden methods
    [extern] is specific to the implementation; an overridden method is
    something we want to define elsewhere, so it doesn't make sense for
    it to inherit this annotation.
    
    Note that if you DO want an [extern] override, you must re-declare
    the method in full; as the override keyword does not accept annotations.

--- cxautogen/classes.c
+++ cxautogen/classes.c
@@ -329,9 +329,11 @@ bool processClass(Class* cls)
         // clone and reset source class
         m = methodClone(m);
         // overriding an abstract method should remove the annotation as it means we explicitly
-        // want to define it here
+        // want to define it here; we remove extern for the same reason
         for (int i = saSize(m->annotations) - 1; i >= 0; --i) {
-            if (saSize(m->annotations.a[i]) == 1 && strEq(m->annotations.a[i].a[0], _S"abstract")) {
+            if (saSize(m->annotations.a[i]) == 1 &&
+                (strEq(m->annotations.a[i].a[0], _S"abstract") ||
+                 strEq(m->annotations.a[i].a[0], _S"extern"))) {
                 saRemove(&m->annotations, i);
             }
         }
