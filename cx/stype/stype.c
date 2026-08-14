Number5ix/cx

commit 6657664aaa5add3bac8d3703a4540b3abd0b3cbf

stype revamp
Major change to the stype system.

- Instead of stype being a 32-bit integer with embedded flags, it is now
  a pointer to a struct. This allows for more extensible types, as well
  as parameterized types for arrays and hashtables in structures.
- Most of the API is compatible, with a few excpetions such as
  stGetId being removed in favor of directly accessing the id field
  of the structure.
- The biggest change is how custom types are defined. See the
  documentation in stype.h for examples of the new method.

cx/stype/stype.c
@@ -1,6 +1,8 @@
 #include "cx/cx.h"
 #include "cx/utils/murmur.h"
 
+#include "stconvert.h"
+
 #include "cx/buffer/stype_buffer.h"
 #include "cx/closure/stype_closure.h"
 #include "cx/container/stype_hashtable.h"
@@ -54,6 +56,13 @@ static intptr stCmp_uint64(stype st, stgeneric gen1, stgeneric gen2, uint32 flag
     return (gen1.st_uint64 < gen2.st_uint64) ? -1 : 1;
 }
 #endif
+
+static intptr stCmp_bool(stype st, stgeneric gen1, stgeneric gen2, uint32 flags)
+{
+    if (gen1.st_bool == gen2.st_bool)
+        return 0;
+    return gen1.st_bool ? 1 : -1;
+}
 
 // 'equal enough' values good enough for general use while taking into account small
 // amounts of floating-point error.
@@ -125,113 +134,152 @@ static void stCopy_none(stype st, _stCopyDest_Anno_(st) stgeneric* dest, _In_ st
     relAssertMsg(_unused_debug_types._unused == 0, "Memory corruption");
 }
 
-// clang-format off
+// Canonical STypeInfo structures for built-in types
 
-alignMem(64) stDtorFunc _stDefaultDtor[256] = {
-    // STCLASS_OPAQUE
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_INT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_UINT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_FLOAT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_PTR
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // 0x50 - 0xdf
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX
-    stDtor_string, stDtor_obj, stDtor_weakref, 0, stDtor_stvar, stDtor_closure, stDtor_buffer, stDtor_struct, stDtor_structptr, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX_CONTAINER
-    stDtor_sarray, stDtor_hashtable, stDtor_cchain, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+const STypeInfo _sti_none = {
+    .name  = "none",
+    .id    = STypeId_none,
+    .flags = 0,
+    .size  = 0,
+    .ops   = { .cmp     = stCmp_none,
+              .hash    = stHash_none,
+              .copy    = stCopy_none,
+              .convert = stConvert_none }
 };
 
-alignMem(64) stCmpFunc _stDefaultCmp[256] = {
-    // STCLASS_OPAQUE
-    stCmp_none, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_INT
-    0, stCmp_int8, stCmp_int16, 0, stCmp_int32, 0, 0, 0, stCmp_int64, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_UINT
-    0, stCmp_uint8, stCmp_uint16, 0, stCmp_uint32, 0, 0, 0, stCmp_uint64, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_FLOAT
-    0, 0, 0, 0, stCmp_float32, 0, 0, 0, stCmp_float64, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_PTR
-    0, 0, 0, 0, sizeof(void*) == sizeof(int32) ? stCmp_ptr : 0, 0, 0, 0,
-    sizeof(void*) == sizeof(int64) ? stCmp_ptr : 0, 0, 0, 0, 0, 0, 0, 0,
-    // 0x50 - 0xdf
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX
-    stCmp_string, stCmp_obj, stCmp_weakref, stCmp_suid, stCmp_stvar, stCmp_closure, stCmp_buffer, stCmp_struct, stCmp_structptr, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX_CONTAINER
-    stCmp_sarray, stCmp_hashtable, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#define ST_BASIC_INFO(type, cvttype) \
+    const STypeInfo _sti_##type = { \
+    .name  = #type, \
+    .id    = STypeId_##type, \
+    .flags = 0, \
+    .size  = sizeof(STStorageType_##type), \
+    .ops = { \
+        .cmp = stCmp_##type, \
+        .convert = stConvert_##cvttype, \
+    }, \
+}
+
+ST_BASIC_INFO(int8, int);
+ST_BASIC_INFO(int16, int);
+ST_BASIC_INFO(int32, int);
+ST_BASIC_INFO(int64, int);
+ST_BASIC_INFO(uint8, int);
+ST_BASIC_INFO(uint16, int);
+ST_BASIC_INFO(uint32, int);
+ST_BASIC_INFO(uint64, int);
+ST_BASIC_INFO(float32, float32);
+ST_BASIC_INFO(float64, float64);
+ST_BASIC_INFO(bool, bool);
+ST_BASIC_INFO(ptr, ptr);
+
+const STypeInfo _sti_string = {
+    .name  = "string",
+    .id    = STypeId_string,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(string)),
+    .ops   = { .dtor    = stDtor_string,
+              .cmp     = stCmp_string,
+              .hash    = stHash_string,
+              .copy    = stCopy_string,
+              .convert = stConvert_string }
 };
 
-alignMem(64) stHashFunc _stDefaultHash[256] = {
-    // STCLASS_OPAQUE
-    stHash_none, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_INT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_UINT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_FLOAT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_PTR
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // 0x50 - 0xdf
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX
-    stHash_string, stHash_obj, 0, 0, stHash_stvar, 0, stHash_buffer, stHash_struct, stHash_structptr, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX_CONTAINER
-    stHash_sarray, stHash_hashtable, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+const STypeInfo _sti_object = {
+    .name  = "object",
+    .id    = STypeId_object,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(object)),
+    .ops   = { .dtor    = stDtor_obj,
+              .cmp     = stCmp_obj,
+              .hash    = stHash_obj,
+              .copy    = stCopy_obj,
+              .convert = stConvert_obj }
 };
 
-alignMem(64) stCopyFunc _stDefaultCopy[256] = {
-    // STCLASS_OPAQUE
-    stCopy_none, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_INT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_UINT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_FLOAT
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_PTR
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // 0x50 - 0xdf
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX
-    stCopy_string, stCopy_obj, stCopy_weakref, 0, stCopy_stvar, stCopy_closure, stCopy_buffer, stCopy_struct, stCopy_structptr, 0, 0, 0, 0, 0, 0, 0,
-    // STCLASS_CX_CONTAINER
-    stCopy_sarray, stCopy_hashtable, stCopy_cchain, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+const STypeInfo _sti_weakref = {
+    .name  = "weakref",
+    .id    = STypeId_weakref,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(weakref)),
+    .ops   = { .dtor = stDtor_weakref, .cmp = stCmp_weakref, .copy = stCopy_weakref }
 };
+
+const STypeInfo _sti_suid = {
+    .name  = "suid",
+    .id    = STypeId_suid,
+    .flags = stFlag(PassPtr),
+    .size  = sizeof(stStorageType(suid)),
+    .ops   = { .cmp = stCmp_suid, .convert = stConvert_suid }
+};
+
+const STypeInfo _sti_stvar = {
+    .name  = "stvar",
+    .id    = STypeId_stvar,
+    .flags = stFlag(PassPtr) | stFlag(Object),
+    .size  = sizeof(stStorageType(stvar)),
+    .ops   = { .dtor    = stDtor_stvar,
+              .cmp     = stCmp_stvar,
+              .hash    = stHash_stvar,
+              .copy    = stCopy_stvar,
+              .convert = stConvert_stvar }
+};
+
+const STypeInfo _sti_closure = {
+    .name  = "closure",
+    .id    = STypeId_closure,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(closure)),
+    .ops   = { .dtor = stDtor_closure, .cmp = stCmp_closure, .copy = stCopy_closure }
+};
+
+const STypeInfo _sti_buffer = {
+    .name  = "buffer",
+    .id    = STypeId_buffer,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(buffer)),
+    .ops   = { .dtor = stDtor_buffer,
+              .cmp  = stCmp_buffer,
+              .hash = stHash_buffer,
+              .copy = stCopy_buffer }
+};
+
+// struct is intentionally omitted. It's not legal to create a bare structure directly. cxautogen
+// automatically creates canonical types for each defined structure that use the struct type ID
+
+const STypeInfo _sti_structptr = {
+    .name  = "structptr",
+    .id    = STypeId_structptr,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(structptr)),
+    .ops   = { .dtor = stDtor_structptr,
+              .cmp  = stCmp_structptr,
+              .hash = stHash_structptr,
+              .copy = stCopy_structptr }
+};
+
+// non-parameterized version of containers
+const STypeInfo _sti_sarray = {
+    .name  = "sarray",
+    .id    = STypeId_sarray,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(sarray)),
+    .ops   = { .cmp = stCmp_sarray, .hash = stHash_sarray, .copy = stCopy_sarray }
+};
+
+const STypeInfo _sti_hashtable = {
+    .name  = "hashtable",
+    .id    = STypeId_hashtable,
+    .flags = stFlag(Object),
+    .size  = sizeof(stStorageType(hashtable)),
+    .ops   = { .cmp = stCmp_hashtable, .hash = stHash_hashtable, .copy = stCopy_hashtable }
+};
+
+// As their size is dynamic, opaque and struct must be constructed as a temporary (or statically by
+// cxautogen in the struct case). Instead we provide default STypeOps here for them to copy.
+
+const STypeOps _stops_opaque = { 0 };
+
+const STypeOps _stops_struct = { .dtor = stDtor_struct,
+                                 .cmp  = stCmp_struct,
+                                 .hash = stHash_struct,
+                                 .copy = stCopy_struct };

cx/container/hashtable.c
@@ -189,8 +189,7 @@ uint32 _htNextSlot(HashTableHeader* hdr, uint32 slot)
 }
 
 _Use_decl_annotations_
-void _htInit(hashtable* out, stype keytype, STypeOps* keyops, stype valtype, STypeOps* valops,
-             uint32 initsz, flags_t flags)
+void _htInit(hashtable* out, stype keytype, stype valtype, uint32 initsz, flags_t flags)
 {
     HashTableHeader* ret;
 
@@ -203,9 +202,10 @@ void _htInit(hashtable* out, stype keytype, STypeOps* keyops, stype valtype, STy
 
     // If we are not using custom ops and this is a POD type, skip all of the copy/destructor
     // logic and just use straight memory copies for speed.
-    if (!keyops && !(stHasFlag(keytype, Object)))
+    if (!keytype->ops.dtor && !keytype->ops.copy && !(stHasFlag(keytype, Object)))
         flags |= HT_RefKeys;
-    if (stGetSize(valtype) == 0 || (!valops && !(stHasFlag(valtype, Object))))
+    if (stGetSize(valtype) == 0 ||
+        (!valtype->ops.dtor && !valtype->ops.copy && !(stHasFlag(valtype, Object))))
         flags |= HT_Ref;
 
     if ((HT_GET_GROW(flags) & HT_GROW_AT_MASK) == HT_GROW_At75) {
@@ -232,23 +232,14 @@ void _htInit(hashtable* out, stype keytype, STypeOps* keyops, stype valtype, STy
 
     uint32 idxsize = initsz * ((flags & HTINT_Metadata) ? HT_IDXENTWITHMETA_SZ : HT_IDXENT_SZ);
 
-    if (keyops || valops) {
-        // need the extended header
-        flags |= HTINT_Extended;
-        ret = xaAlloc(HTABLE_HDRSIZE + idxsize);
-        memset(&ret->keytypeops, 0, sizeof(ret->keytypeops));
-        memset(&ret->valtypeops, 0, sizeof(ret->valtypeops));
-    } else {
-        // revel in the evil
-        ret = (HashTableHeader*)((uintptr)xaAlloc((HTABLE_HDRSIZE + idxsize) - HT_SMALLHDR_OFFSET) -
-                                 HT_SMALLHDR_OFFSET);
-    }
+    ret = xaAlloc(HTABLE_HDRSIZE + idxsize);
 
     ret->idxsz   = initsz;
     ret->idxused = ret->storused = ret->valid = 0;
 
-    ret->keytype = keytype;
-    ret->valtype = valtype;
+    ret->tbltype = NULL;   // will be filled in lazily if requested
+    ret->keytype = stCanonical(keytype);
+    ret->valtype = stCanonical(valtype);
     ret->flags   = flags;
 
     // prep the first storage chunk
@@ -259,17 +250,43 @@ void _htInit(hashtable* out, stype keytype, STypeOps* keyops, stype valtype, STy
     // converts to a boolean
     ret->storused = 1;
 
-    if (keyops) {
-        ret->keytypeops = *keyops;
-    }
-    if (valops) {
-        ret->valtypeops = *valops;
-    }
-
     // fill index, metadata with empty sentinels
     memset(ret->index, 0, idxsize);
 
     *out = (hashtable)&ret->index[0];
+}
+
+_Use_decl_annotations_
+void _htInitFromType(hashtable* out, stype tbltype, uint32 initsz, flags_t flags)
+{
+    devAssert(tbltype);
+    devAssert(tbltype->id == stTypeId(hashtable));
+
+    // tbltype SHOULD already be canonical, but just in case
+    tbltype = stCanonical(tbltype);
+
+    stype keytype = tbltype->param[0];
+    stype valtype = tbltype->param[1];
+    devAssert(keytype && valtype);
+
+    _htInit(out, keytype, valtype, initsz, flags);
+
+    HashTableHeader* ret = _htHdr(*out);
+    ret->tbltype         = tbltype;
+}
+
+_Use_decl_annotations_
+stype htType(hashtable ref)
+{
+    HashTableHeader* hdr = _htHdr(ref);
+    if (!hdr->tbltype) {
+        STypeInfo tmp = stTypeInfo(hashtable);   // copy hashtable type to use as a base
+        tmp.flags |= stFlag(Temporary);
+        tmp.param[0] = hdr->keytype;
+        tmp.param[1] = hdr->valtype;
+        hdr->tbltype = stCanonical(&tmp);   // this will register the type if needed
+    }
+    return hdr->tbltype;
 }
 
 static _meta_inline uint32 clampHash(_In_ HashTableHeader* hdr, uint32 hash)
@@ -289,7 +306,7 @@ htFastFindIndex(_In_ HashTableHeader* hdr, _In_ stgeneric key, uint8* metaOut)
     uint32 probes   = 1;
 
     uint32* idx = hdr->index;
-    uint32 hash = _stHash(hdr->keytype, HDRKEYOPS(hdr), key, opsflags);
+    uint32 hash = _stHash(hdr->keytype, key, opsflags);
     if (metaOut)
         *metaOut = hash >> 24;
 
@@ -331,16 +348,10 @@ _Ret_valid_ static hashtable _htClone(_In_ hashtable htbl, uint32 minsz, bool mo
 
     uint32 idxsize = newsz * ((newflags & HTINT_Metadata) ? HT_IDXENTWITHMETA_SZ : HT_IDXENT_SZ);
     // make a new table to copy stuff into
-    if (hdr->flags & HTINT_Extended) {
-        // need the extended header
-        nhdr = xaAlloc(HTABLE_HDRSIZE + idxsize);
-    } else {
-        nhdr = (HashTableHeader*)((uintptr)xaAlloc((HTABLE_HDRSIZE + idxsize) -
-                                                   HT_SMALLHDR_OFFSET) -
-                                  HT_SMALLHDR_OFFSET);
-    }
-    ntbl = (hashtable)&nhdr->index[0];
+    nhdr           = xaAlloc(HTABLE_HDRSIZE + idxsize);
+    ntbl           = (hashtable)&nhdr->index[0];
 
+    nhdr->tbltype = hdr->tbltype;
     nhdr->keytype = hdr->keytype;
     nhdr->valtype = hdr->valtype;
     nhdr->idxsz   = newsz;
@@ -354,11 +365,6 @@ _Ret_valid_ static hashtable _htClone(_In_ hashtable htbl, uint32 minsz, bool mo
         nhdr->flags = newflags | HT_Ref | HT_RefKeys;
     } else {
         nhdr->flags = newflags;
-    }
-
-    if (hdr->flags & HTINT_Extended) {
-        nhdr->keytypeops = hdr->keytypeops;
-        nhdr->valtypeops = hdr->valtypeops;
     }
 
     // initialize new index
@@ -432,12 +438,7 @@ void htReindex(hashtable* htbl, uint32 minsz)
     *htbl = _htClone(*htbl, minsz, true, false);
 
     // free old table
-    if (hdr->flags & HTINT_Extended) {
-        xaFree(hdr);
-    } else {
-        void* smbase = (void*)((uintptr_t)hdr + HT_SMALLHDR_OFFSET);
-        xaFree(smbase);
-    }
+    xaFree(hdr);
 }
 
 _Use_decl_annotations_
@@ -459,12 +460,7 @@ void htRepack(hashtable* htbl)
     xaFree(hdr->chunks);
 
     // free old table
-    if (hdr->flags & HTINT_Extended) {
-        xaFree(hdr);
-    } else {
-        void* smbase = (void*)((uintptr_t)hdr + HT_SMALLHDR_OFFSET);
-        xaFree(smbase);
-    }
+    xaFree(hdr);
 }
 
 static void htGrowIndex(_Inout_ptr_ hashtable* htbl)
@@ -492,7 +488,7 @@ static bool htFindIndex(_In_ HashTableHeader* hdr, _In_ stgeneric key, _Out_opt_
         *deletedOut = hashIndexDeleted;
 
     uint32* idx = hdr->index;
-    uint32 hash = _stHash(hdr->keytype, HDRKEYOPS(hdr), key, opsflags);
+    uint32 hash = _stHash(hdr->keytype, key, opsflags);
     uint32 nexthash;
 
     // for fast rejection, the metadata is the high 8 bits of the unclamped hash
@@ -525,7 +521,6 @@ static bool htFindIndex(_In_ HashTableHeader* hdr, _In_ stgeneric key, _Out_opt_
                 // potential match, do full key compare
                 void* skey = HT_SLOT_KEY_PTR(hdr, slot);
                 if (_stCmp(hdr->keytype,
-                           HDRKEYOPS(hdr),
                            key,
                            stStored(hdr->keytype, skey),
                            opsflags | ST_Equality) == 0) {
@@ -582,18 +577,14 @@ static void htSetValueInternal(_Inout_ HashTableHeader* hdr, uint32 slot, _When_
         // destroy source
         if (hdr->flags &
             HT_Ref)   // this combination doesn't make much sense, but should respect it
-            _stDestroy(hdr->valtype, HDRVALOPS(hdr), val, 0);
+            _stDestroy(hdr->valtype, val, 0);
         else if (stGetSize(hdr->valtype) == sizeof(void*))
             val->st_ptr = 0;   // if this is a pointer-sized element, clear it out
         return;
     }
 
     if (!(hdr->flags & HT_Ref))
-        _stCopy(hdr->valtype,
-                HDRVALOPS(hdr),
-                stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)),
-                *val,
-                0);
+        _stCopy(hdr->valtype, stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)), *val, 0);
     else
         memcpy(HT_SLOT_VAL_PTR(hdr, slot), stGenPtr(hdr->valtype, *val), stGetSize(hdr->valtype));
 }
@@ -615,17 +606,14 @@ static uint32 _htInsertInternal(hashtable* htbl, stgeneric key, stgeneric* val, 
         if (flags & HT_Ignore) {
             // already exists and set to ignore, so do not set value
             if (flags & HTINT_Consume)
-                _stDestroy(hdr->valtype, HDRVALOPS(hdr), val, 0);
+                _stDestroy(hdr->valtype, val, 0);
             return slot;
         }
 
         if (stGetSize(hdr->valtype) > 0) {
             // replacing existing value, destroy it first if necessary
             if (!(hdr->flags & HT_Ref))
-                _stDestroy(hdr->valtype,
-                           HDRVALOPS(hdr),
-                           stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)),
-                           0);
+                _stDestroy(hdr->valtype, stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)), 0);
 
             htSetValueInternal(hdr, slot, val, flags & HTINT_Consume);
         }
@@ -644,11 +632,7 @@ static uint32 _htInsertInternal(hashtable* htbl, stgeneric key, stgeneric* val, 
 
     // set key
     if (!(hdr->flags & HT_RefKeys))
-        _stCopy(hdr->keytype,
-                HDRKEYOPS(hdr),
-                stStoredPtr(hdr->keytype, HT_SLOT_KEY_PTR(hdr, slot)),
-                key,
-                0);
+        _stCopy(hdr->keytype, stStoredPtr(hdr->keytype, HT_SLOT_KEY_PTR(hdr, slot)), key, 0);
     else
         memcpy(HT_SLOT_KEY_PTR(hdr, slot), stGenPtr(hdr->keytype, key), stGetSize(hdr->keytype));
 
@@ -702,15 +686,9 @@ static void _htClear(_Inout_ HashTableHeader* hdr, bool reuse)
         // destroy data in storage slots
         for (uint32 slot = _htNextSlot(hdr, 0); slot != 0; slot = _htNextSlot(hdr, slot)) {
             if (!(hdr->flags & HT_RefKeys))
-                _stDestroy(hdr->keytype,
-                           HDRKEYOPS(hdr),
-                           stStoredPtr(hdr->keytype, HT_SLOT_KEY_PTR(hdr, slot)),
-                           0);
+                _stDestroy(hdr->keytype, stStoredPtr(hdr->keytype, HT_SLOT_KEY_PTR(hdr, slot)), 0);
             if (!(hdr->flags & HT_Ref))
-                _stDestroy(hdr->valtype,
-                           HDRVALOPS(hdr),
-                           stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)),
-                           0);
+                _stDestroy(hdr->valtype, stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)), 0);
         }
     }
 
@@ -778,12 +756,7 @@ void htDestroy(hashtable* htbl)
     if (stGetSize(hdr->valtype) > 0)
         xaFree(hdr->valstorage);
     xaFree(hdr->chunks);
-    if (hdr->flags & HTINT_Extended) {
-        xaFree(hdr);
-    } else {
-        void* smbase = (void*)((uintptr_t)hdr + HT_SMALLHDR_OFFSET);
-        xaFree(smbase);
-    }
+    xaFree(hdr);
     *htbl = NULL;
 }
 
@@ -804,11 +777,7 @@ htelem _htFind(hashtable htbl, stgeneric key, stgeneric* val, flags_t flags)
                        HT_SLOT_VAL_PTR(hdr, slot),
                        stGetSize(hdr->valtype));
             else
-                _stCopy(hdr->valtype,
-                        HDRVALOPS(hdr),
-                        val,
-                        stStored(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)),
-                        0);
+                _stCopy(hdr->valtype, val, stStored(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)), 0);
         }
         return slot;
     }
@@ -841,15 +810,9 @@ bool _htExtract(hashtable* htbl, stgeneric key, stgeneric* val)
                    HT_SLOT_VAL_PTR(hdr, slot),
                    stGetSize(hdr->valtype));
         else if (!(hdr->flags & HT_Ref))
-            _stDestroy(hdr->valtype,
-                       HDRVALOPS(hdr),
-                       stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)),
-                       0);
+            _stDestroy(hdr->valtype, stStoredPtr(hdr->valtype, HT_SLOT_VAL_PTR(hdr, slot)), 0);
         if (!(hdr->flags & HT_RefKeys))
-            _stDestroy(hdr->keytype,
-                       HDRKEYOPS(hdr),
-                       stStoredPtr(hdr->keytype, HT_SLOT_KEY_PTR(hdr, slot)),
-                       0);
+            _stDestroy(hdr->keytype, stStoredPtr(hdr->keytype, HT_SLOT_KEY_PTR(hdr, slot)), 0);
 
         // mark deleted in index
         hdr->index[idxent]     = hashIndexDeleted;

commit b92627c27d8031a7a463df9cef8812bc2de83439

Fix contained sarray and hashtable not being destroyed
Missing destructor in the STypeInfo. Regression introduced during the
revamp in commit 6657664aaa5add3bac8d3703a4540b3abd0b3cbf.

cx/stype/stype.c
@@ -268,16 +268,21 @@ const STypeInfo _sti_sarray = {
     .id    = STypeId_sarray,
     .flags = stFlag(Object),
     .size  = sizeof(stStorageType(sarray)),
-    .ops   = { .cmp = stCmp_sarray, .hash = stHash_sarray, .copy = stCopy_sarray }
+    .ops   = { .dtor = stDtor_sarray,
+              .cmp  = stCmp_sarray,
+              .hash = stHash_sarray,
+              .copy = stCopy_sarray }
 };
 
-STR_CONSTR(hashtable, "hashtable");
 const STypeInfo _sti_hashtable = {
     .name  = _SR(hashtable),
     .id    = STypeId_hashtable,
     .flags = stFlag(Object),
     .size  = sizeof(stStorageType(hashtable)),
-    .ops   = { .cmp = stCmp_hashtable, .hash = stHash_hashtable, .copy = stCopy_hashtable }
+    .ops   = { .dtor = stDtor_hashtable,
+              .cmp  = stCmp_hashtable,
+              .hash = stHash_hashtable,
+              .copy = stCopy_hashtable }
 };
 
 // As their size is dynamic, opaque and struct must be constructed as a temporary (or statically by